5a212c7fdc5830c995ad1809cc7aef3a89101cf0
Ígor Bonadio <igorbonadio@gmail.com>
2010-07-27 18:12:54 -0300

    char and string functions added to core library

---
+++ lib/vm/core/pchar.rb
@@ -0,0 +1,13 @@
+module PLang
+  module VM
+    module PFunctions
+      def add_to_interpreter_char_functions
+        object_message "{char: x}", :ord do |object|
+          plambda do |value|
+            PObject.new(:integer, [object.params[0].ord])
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/vm/core/pstring.rb
@@ -0,0 +1,27 @@
+module PLang
+  module VM
+    module PFunctions
+      def add_to_interpreter_string_functions
+        
+        object_message "{string: x}", :at do |object|
+          plambda "{integer: y}" do |value|
+            PObject.new(:char, [object.params[0][value[0].params[0]]])
+          end
+        end
+        
+        object_message "{string: x}", :concat do |object|
+          plambda "{string: y}" do |value|
+            PObject.new(:string, [object.params[0] + value[0].params[0]])
+          end
+        end
+        
+        object_message "x", :to_s do |object|
+          plambda do |value|
+            PObject.new(:string, [object.params[0].to_s])
+          end
+        end
+        
+      end
+    end
+  end
+end
