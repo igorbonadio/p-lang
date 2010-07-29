310dd1fa56cb3e48c4b6819e2ab8557937744d4c
Ígor Bonadio <igorbonadio@gmail.com>
2010-07-28 22:49:19 -0300

    io functions added

---
+++ lib/vm/core/pio.rb
@@ -0,0 +1,16 @@
+module PLang
+  module VM
+    module PFunctions
+      def add_to_interpreter_io_functions
+        
+        var :print, (plambda "x" do |params|
+          puts params[0].to_s
+        end)
+
+        var :read, (plambda do
+          STDIN.gets
+        end)
+      end
+    end
+  end
+end
--- lib/vm/pobject.rb
+++ lib/vm/pobject.rb
@@ -8,6 +8,26 @@ module PLang
         @id = id
         @params = params
       end
+
+      def to_s
+        case @id
+          when :integer, :decimal, :char, :string, :boolean
+            return params[0]
+          else
+            str = "{#{@id}"
+            if @params.length > 0
+              str += ": "
+              @params.each do |param|
+                str += "#{param.to_s}, "
+              end
+              str[-2] = "}"
+              str
+            else
+              str += "}"
+            end
+            str.strip
+        end
+      end
     end
   end
 end
